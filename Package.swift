b24acafe529c07a71971595f0cef3d908ed56315
Liam <liam_i@163.com>
2024-01-11 17:43:39 +0800

    fix: Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "EmptyDataSet", path: "Sources"))
+        .target(name: "EmptyDataSet", path: "Sources")
 //        .testTarget(name: "EmptyDataSetTests", dependencies: ["EmptyDataSet"]),
     ]
 )
